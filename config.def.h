75ed4a9d1e35a1da010f3e98a22f559e04676a2c
GabzDK <gabriel3047.eufrazio@gmail.com>
2024-07-09 08:47:56 -0300

    backup

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -13,7 +13,7 @@ static const char col_gray1[]       = "#282828";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#ebdbb2";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#458588";
+static const char col_cyan[]        = "#770000";
 
 static const unsigned int baralpha = 140;
 static const unsigned int borderalpha = 140;
