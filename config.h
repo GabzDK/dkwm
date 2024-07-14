bd90eccd79e612c48e0a419d7ba619936339b9f7
GabzDK <gabriel3047.eufrazio@gmail.com>
2024-07-14 19:46:00 -0300

    removed splistatus and added vacant tags

--- config.h
+++ config.h
@@ -1,29 +1,28 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 10;        /* gaps between windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int gappx     = 15;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int splitstatus        = 1;        /* 1 for split status items */
-static const char *splitdelim        = ";";       /* Character used for separating status */
 static const char *fonts[]          = { "MesloLGLDZ Nerd Font:size=10" };
-static const char col_gray1[]       = "#282828";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#ebdbb2";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#770000";
+static char normbgcolor[]           = "#282828";
+static char normbordercolor[]       = "#444444";
+static char normfgcolor[]           = "#bbbbbb";
+static char selfgcolor[]            = "#eeeeee";
+static char selbordercolor[]        = "#770000";
+static char selbgcolor[]            = "#005577";
+static const unsigned int baralpha = OPAQUE;
+static const unsigned int borderalpha = OPAQUE;
 
-static const unsigned int baralpha = 100;
-static const unsigned int borderalpha = 100;
-
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+static const char *colors[][3] = {
+       /*               fg           bg           border   */
+       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
+
 static const unsigned int alphas[][3]      = {
     /*               fg      bg        border*/
     [SchemeNorm] = { OPAQUE, baralpha, borderalpha },
@@ -44,8 +43,8 @@ static const char *const autostart[] = {
 };
 
 /* tagging */
-//static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };:
-static const char *tags[] = {"ένα","δύο","τρία", "τέσσερα", "πέντε", "έξι" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+//static const char *tags[] = {"ένα","δύο","τρία", "τέσσερα", "πέντε", "έξι" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -125,7 +124,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,	                    XK_q,      killclient,     {0} },
-    { MODKEY,             XK_z,      unfloatvisible, {.v = &layouts[0]} },
+    { MODKEY,                       XK_z,      unfloatvisible, {.v = &layouts[0]} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
